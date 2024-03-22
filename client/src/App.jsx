3f4b79bc0d183cabdbd854c2333d562f0b0bc872
Harshit-Jajoria <harshitjajoria66@gmail.com>
2024-03-22 13:27:33 +0530

    edits

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import ReactGA4 from "react-ga4";
-
+import { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ReactGA4 from 'react-ga4';
+import Home from './pages/Home';
 
 const initializeGA4 = async () => {
   const measurementId = 'G-3Q5XMQGSMG';
@@ -15,36 +13,17 @@ const initializeGA4 = async () => {
 };
 
 function App() {
-  const [count, setCount] = useState(0)
-
   useEffect(() => {
     initializeGA4();
   }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          gacount is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
